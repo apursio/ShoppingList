eb14d924748e6685ed67303483b78f356059663a
Anna Pursio <anna.pursio@student.lab.fi>
2024-03-20 08:59:39 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import {AppRegistry} from 'react-native';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
